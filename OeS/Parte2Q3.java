b121e597093ac9561d74746faffc8e1de5e25232
NeilAllan2004 <113200314+NeilAllan2004@users.noreply.github.com>
2023-03-05 12:11:50 -0300

    Questão 3 da Lista 2

--- OeS/Parte2Q3.java
+++ OeS/Parte2Q3.java
@@ -1,5 +1,5 @@
 public class Q3 {
-  public static void main(string[] args) {
+  public static void main(String[] args) {
     int x=80;
     int y = (x % 2 == 0 ? x/2 : 3 * x + 1);
     System.out.println("O valor de y é "+y);
