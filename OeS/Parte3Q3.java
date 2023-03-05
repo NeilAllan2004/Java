92fafe2956783f498f2863dd95090b04e695c10c
NeilAllan2004 <113200314+NeilAllan2004@users.noreply.github.com>
2023-03-05 11:49:21 -0300

    Questão 3 da Lista 3

---
+++ OeS/Parte3Q3.java
@@ -0,0 +1,12 @@
+public class Q3 {
+  public static void main(string[] args) {
+    int Valor;
+    for(int x=1;x<=10;x++){
+      Valor=1;
+      for(int y=1;y<=x;y++){
+        Valor=Valor*y;
+      }
+      System.out.println(Valor);
+    }
+  }
+}
