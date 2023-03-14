1f128f3209bf9be5b0380e48ed67481d3319d0aa
NeilAllan2004 <113200314+NeilAllan2004@users.noreply.github.com>
2023-03-14 17:24:15 -0300

    Questão 5 do EX3

---
+++ AtividadeVisibilidadeEncapsulamento/Q5.java
@@ -0,0 +1,46 @@
+package AtividadeVisibilidadeEncapsulamento.;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Funcionario {
+    private String Nome;
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    private Date dataAdmissao;
+    private double salario;
+    
+    public String getNome(){
+        return this.Nome;
+    }
+
+    public void setNome(String Nome){
+        this.Nome = Nome;
+    }
+
+    public double getsalario(){
+        return this.salario;
+    }
+
+    public void setsalario(double salario){
+        if(salario>=1100){
+            this.salario = salario;
+        }
+        else{
+            System.out.print("Não foi possível adicionar esse salário");
+        }
+    }
+
+    public Date getdataAdmissao(){
+        return this.dataAdmissao;
+    }
+
+    public void setdataAdmissao(String DataAdmissao){
+        try {
+            this.dataAdmissao= sdf.parse(DataAdmissao);
+        } catch (ParseException ex) {
+            Logger.getLogger(Funcionario.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
