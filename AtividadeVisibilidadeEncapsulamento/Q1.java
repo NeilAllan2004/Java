f69afac93943824343fd0659961e1e84350263a9
NeilAllan2004 <113200314+NeilAllan2004@users.noreply.github.com>
2023-03-14 16:32:18 -0300

    Questão 1 do EX3

---
+++ AtividadeVisibilidadeEncapsulamento/Q1.java
@@ -0,0 +1,8 @@
+package AtividadeVisibilidadeEncapsulamento.;
+import java.text.SimpleDateFormat;
+
+public class Funcionario {
+    private String Nome;
+    private SimpleDateFormat dataAdmissao = new SimpleDateFormat("dd/MM/yyyy");
+    private float salario;
+}
