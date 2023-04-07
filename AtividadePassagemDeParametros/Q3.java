f4af37c390e1b5271fe79a479ef40301aa0ae595
NeilAllan2004 <113200314+NeilAllan2004@users.noreply.github.com>
2023-04-07 16:59:46 -0300

    Questão 3 do EX8

---
+++ AtividadePassagemDeParametros/Q3.java
@@ -0,0 +1,16 @@
+package atividadepassagemdeparametros;
+
+public class Q3 {
+
+    public static void main(String[] args) {
+        int valor=10;
+        MudarValor(valor);
+        System.out.println("Valor: "+valor);
+        
+    }
+    
+    public static void MudarValor(int valor){
+        valor=15;
+    }
+    
+}
