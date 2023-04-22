94030c6c0625fe0b79ac66478df87aa789cb95a9
NeilAllan2004 <113200314+NeilAllan2004@users.noreply.github.com>
2023-04-22 17:53:45 -0300

    Classe Execução da Questão 3 da Lista 2 do EX9

---
+++ AtividadeExcecoes/AtividadeExcecoes2/Execucao.java
@@ -0,0 +1,11 @@
+public class Execucao {
+    
+    public static void main(String[] args) {
+        Data Teste1 = new Data(31,2,2004);
+        Data Teste2 = new Data(31,11,2004);
+        Data Teste3 = new Data(32,2,2004);
+        Data Teste4 = new Data(31,13,2004);
+        Data Teste5 = new Data(31,2,2004);
+        Data Teste6 = new Data(31,12,2004);
+    }
+}
